65153ee17c080f543dc4f7a772b4afa7fc1e4f74
Anna Lyubinska <anna.lyubinskagmail.com>
2025-03-20 11:42:30 +0100

    feat:add core models in app kitchen
    Add model Cook inherited from ABStrakrUser, model Dish, model Ingredient

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,4 +1,45 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from kitchen_service.settings import AUTH_USER_MODEL
+
 
 class DishType(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE,related_name="dishes")
+    cooks = models.ManyToManyField(AUTH_USER_MODEL, related_name="dishes")
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    dish = models.ManyToManyField(Dish, related_name="ingredients")
+
+    def __str__(self):
+        return self.name
+
+
