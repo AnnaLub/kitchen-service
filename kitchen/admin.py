9c2dd6406ddf69d30cda510badf5cc4930137123
Anna Lyubinska <anna.lyubinskagmail.com>
2025-03-20 14:14:15 +0100

    feat: add models to admin panel
    Register Cook and  DishType, Dish with list_display, list_filter, search_fields

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from kitchen.models import Cook, DishType, Dish
+
+admin.site.register(Cook, UserAdmin)
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    list_filter = ["name"]
+    search_fields = ["name"]
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "dish_type", "price","description"]
+    list_filter = ["name", "dish_type", "price"]
+    search_fields = ["name"]
