0a5a8978d0810abf41e8da8fd41a1bc1d351d383
Anna Lyubinska <anna.lyubinskagmail.com>
2025-03-21 20:50:46 +0100

    feat: add login_required_url

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -118,9 +118,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "kitchen.Cook"
+
+LOGIN_REDIRECT_URL = "/"
