e946e509ddce18f0ca3545e19dd67e420b740f71
Anna Lyubinska <anna.lyubinskagmail.com>
2025-03-21 11:54:03 +0100

    feat: add models to admin panel
    Register Ingredient and change the registration of the model Cook

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from kitchen.models import Cook, DishType, Dish
+from kitchen.models import Cook, DishType, Dish, Ingredient
 
-admin.site.register(Cook, UserAdmin)
+
 
 @admin.register(DishType)
 class DishTypeAdmin(admin.ModelAdmin):
@@ -11,8 +11,25 @@ class DishTypeAdmin(admin.ModelAdmin):
     list_filter = ["name"]
     search_fields = ["name"]
 
+
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
-    list_display = ["name", "dish_type", "price","description"]
+    list_display = ["name", "dish_type", "price", "description"]
     list_filter = ["name", "dish_type", "price"]
     search_fields = ["name"]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    list_filter = ["name"]
+    search_fields = ["name"]
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (("Additional Information",{"fields": ("years_of_experience",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional Information",{"fields":
+                                                                              ("first_name",
+                                                                               "last_name",
+                                                                               "years_of_experience",)}),)
