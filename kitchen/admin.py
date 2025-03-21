a5d13c6a8244616da1036b61363a6564d791c343
Anna Lyubinska <anna.lyubinskagmail.com>
2025-03-21 12:09:50 +0100

    style: format models to flake8

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -4,7 +4,6 @@ from django.contrib.auth.admin import UserAdmin
 from kitchen.models import Cook, DishType, Dish, Ingredient
 
 
-
 @admin.register(DishType)
 class DishTypeAdmin(admin.ModelAdmin):
     list_display = ["name"]
@@ -25,11 +24,22 @@ class IngredientAdmin(admin.ModelAdmin):
     list_filter = ["name"]
     search_fields = ["name"]
 
+
 @admin.register(Cook)
 class CookAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("years_of_experience",)
-    fieldsets = UserAdmin.fieldsets + (("Additional Information",{"fields": ("years_of_experience",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + (("Additional Information",{"fields":
-                                                                              ("first_name",
-                                                                               "last_name",
-                                                                               "years_of_experience",)}),)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Information", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional Information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "years_of_experience",
+                )
+            },
+        ),
+    )
