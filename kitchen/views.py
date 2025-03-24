c1b841e06492b9dd6a51287b7780a61c61d8fce0
Anna Lyubinska <anna.lyubinskagmail.com>
2025-03-24 15:14:10 +0100

    feat: add all ListView for project
    add DishTypeListView, DishListView, CookListView, IngredientListView

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
 from .models import DishType, Dish, Cook, Ingredient
 
 # Create your views here.
@@ -21,3 +24,30 @@ def index(request:HttpRequest) -> HttpResponse:
     }
 
     return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_type_list.html"
+    context_object_name = "dish_type_list"
+    paginate_by = 5
+
+
+class DishListView(LoginRequiredMixin, generic.ListView):
+    model = Dish
+    template_name = "kitchen/dish_list.html"
+    queryset = Dish.objects.all().select_related("dish_type")
+    paginate_by = 5
+
+
+class CookListView(LoginRequiredMixin, generic.ListView):
+    model = Cook
+    paginate_by = 5
+    template_name = "kitchen/cook_list.html"
+
+
+class IngredientListView(LoginRequiredMixin, generic.ListView):
+    model = Ingredient
+    paginate_by = 5
+    template_name = "kitchen/ingredient_list.html"
+    queryset = Ingredient.objects.all().prefetch_related("dish")
