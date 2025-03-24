13817f98357d877488904bd780b2b528aa81fba2
Anna Lyubinska <anna.lyubinskagmail.com>
2025-03-24 15:03:50 +0100

    feat: add routs for all ListView

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,10 +1,15 @@
 
 from django.urls import path
 
-from .views import  index
+from .models import Ingredient
+from .views import  index, DishTypeListView, DishListView, CookListView, IngredientListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish_types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("cooks/", CookListView.as_view(), name="cook-list"),
+    path("ingredients", IngredientListView.as_view(), name="ingredients-list"),
 ]
 
 app_name = "kitchen"
