6564af8556b2a03f623ecc5e3d2e4f3aebeac934
Anna Lyubinska <anna.lyubinskagmail.com>
2025-03-26 21:04:59 +0100

    feat: add CookCreationForm based on UserCreationForm
    add validate mathod of year_of_experience

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
-from kitchen.models import Dish, Ingredient
+from kitchen.models import Dish, Ingredient, Cook
 
 
 class IngredientForm(forms.ModelForm):
@@ -11,4 +12,23 @@ class IngredientForm(forms.ModelForm):
 
     class Meta:
         model = Ingredient
-        fields = '__all__'
+        fields = "__all__"
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta:
+        model = Cook
+        fields = (UserCreationForm.Meta.fields +
+                  ("first_name","last_name", "years_of_experience"))
+
+    def clean_years_of_experience(self):
+        years_of_experience = self.cleaned_data['years_of_experience']
+        return validate_year_of_experience(years_of_experience)
+
+
+def validate_year_of_experience(value: int):
+    if value < 0:
+        raise forms.ValidationError("year of experience must be positive")
+    elif value > 30:
+        raise forms.ValidationError("year of experience must be less than 30")
+    return value
