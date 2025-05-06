a3529a9f5da1597b4d02caf2ddedc3c15ff6513c
Anna Lyubinska <anna.lyubinskagmail.com>
2025-05-06 20:16:51 +0200

    update settings & requirements.txt

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.db.models import IntegerField
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zaf!b)v^)w4!8vskd@hzt-7vv^j8z9vaqs!v@mok6sb=k1rt07"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",
+                            "django-insecure-zaf!b)v^)w4!8vskd@hzt-7vv^j8z9vaqs!v@mok6sb=k1rt07")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.01"]
 
 
 # Application definition
