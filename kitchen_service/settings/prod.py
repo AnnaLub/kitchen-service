81418860b4f727a5eb2177a19a13177bd2b40908
Anna Lyubinska <anna.lyubinskagmail.com>
2025-05-11 19:13:29 +0200

    add variable render_external_hostname

--- kitchen_service/settings/prod.py
+++ kitchen_service/settings/prod.py
@@ -2,7 +2,11 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Database
